ca649b422a3cb154d0c23c77e6955f58cf49f8e4
bodolawale <bodolawale@gmail.com>
2021-06-24 21:22:55 +0100

    added proto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const grcpOptions: GrpcOptions = {
+    transport: Transport.GRPC,
+    options: {
+      package: 'user',
+      protoPath: join(__dirname, 'proto/user/user.service.proto'),
+      url: `localhost:${process.env.GRPC_PORT}`,
+    },
+  };
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice(grcpOptions);
+
+  await app.startAllMicroservicesAsync();
   await app.listen(3000);
 }
 bootstrap();
