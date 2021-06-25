f5e2aadfa8e139b8f091a7fce0f4971edf2cb2bf
bodolawale <bodolawale@gmail.com>
2021-06-25 08:46:10 +0100

    fixed all proto errors

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
+import {
+  CreateUserResponse,
+  CreateUserPayload,
+} from './../../generated_proto/user/user_pb';
 import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata } from 'grpc';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @GrpcMethod('UserService')
+  createUser(
+    body: CreateUserPayload.AsObject,
+    metadata: Metadata,
+  ): Promise<CreateUserResponse.AsObject> {
+    const a: any = 1;
+    return a;
+  }
+}
