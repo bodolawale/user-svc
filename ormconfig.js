8adec4197d80da39c1139c16abf70471489a0d12
bodolawale <bodolawale@gmail.com>
2021-06-25 16:04:48 +0100

    added ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: Number.parseInt(process.env.PG_PORT, 2),
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+  },
+};
